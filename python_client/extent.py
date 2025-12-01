bfbab4b7879b91ea919fa62cfe9cbaa302538505
Keira Adams <keira.f.adams@gmail.com>
2025-12-01 12:58:45 -0800

    WiP. Extent and serialization working, virtual object WiP.

--- python_client/extent.py
+++ python_client/extent.py
@@ -1,16 +1,36 @@
 class Extent(object):
-    def __init__(self,lba,len):
+    def __init__(self,lba,ext_len):
         """
-
-        :param lba:
-        :param len:
+        :param lba: integer, logical block address
+        :param len: length in number of blocks
         """
-        self.lba=lba
-        self.len=len
+        self.lba = lba
+        self.ext_len = ext_len
 
     def serialize(self):
         """
+        Returns a binary serialized version of the extent data.
 
-        :return:
+        :return: serialized byte object
         """
-        pass
+        serialized_lba = self.lba.to_bytes(8, byteorder='little', signed=False)
+        serialized_ext_len = self.ext_len.to_bytes(8, byteorder='little', signed=False)
+        ser_bytes =(serialized_lba + serialized_ext_len)
+
+        return ser_bytes
+
+def deserialize(ser_bytes):
+    """
+    Returns a new extent object from the provided serialized bytes.
+
+    :param ser_bytes: serialized extent bytes, should be 16 bytes long
+    :return: extent object
+    """
+    lba = int.from_bytes(ser_bytes[0:7], byteorder='little', signed=False)
+    ext_len = int.from_bytes(ser_bytes[8:15], byteorder='little', signed=False)
+
+    new_ext = Extent(lba,ext_len)
+
+    return new_ext
+
+
--- python_client/vobj.py
+++ python_client/vobj.py
@@ -1,15 +1,48 @@
 import pyfiemap
+import extent
 
 class VirtualObject(object):
-    def __init__(self, fpath, input=True):
+    def __init__(self, fpath, blk_sz, input=True):
+        # TODO elise said something something error checking dont blow up the system or put
+        # your toaster in the bathtub meh meh meh
         self.input = input
-        self.nr_exts = -1
+        self.nr_exts = 0
         self.ext_list = []
+        self.blk_sz = blk_sz
+
+        self._convert_to_exts(fpath)
 
     def _convert_to_exts(self, fpath):
         """
-        :param fpath:
+        :param fpath: python string, full path to file we wish to create a virtual object out of.
+
         :return:
         """
 
+        raw_ext_list = pyfiemap.get_ext_list(fpath)
+
+        for ext in raw_ext_list:
+            lba = ext[2]/self.blk_sz
+            nr_blks = ext[3]/self.blk_sz
+            new_ext = extent.Extent(lba, nr_blks) # Hooray! A successful autocomplete that totally needed a nuclear reactor and an LLM!
+            self.ext_list.append(new_ext)
+            self.nr_exts += 1
+
+
+    def serialize(self):
+        """
+        Convert virtual object to a python byte array
+
+        :return:
+        """
         pass
+
+
+def deserialize(serialized_bytes):
+    """
+    Converts a seralized virtual object into a python virtual object
+
+    :param serialized_bytes:
+    :return:
+    """
+    pass
